f7da5efa8c0799f4fea4cdbc59c866e569e8c456
nagiyu <otonagiyu@gmail.com>
2024-12-20 16:06:02 +0900

    Update path aliases in tsconfig and vite config for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig(({ command }) => {
     },
     resolve: {
       alias: {
-        '@auth': path.resolve(__dirname, 'common/auth/client'), // 絶対パスで`@`を使えるようにする TODO: 共通のファイルができればパスを変える
+        '@auth': path.resolve(__dirname, 'auth/client'), // 絶対パスで`@`を使えるようにする TODO: 共通のファイルができればパスを変える
       },
     },
     server: {
