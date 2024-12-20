5a3167a51ec98e0bf7c8c857ac5a13cba47d44f3
nagiyu <otonagiyu@gmail.com>
2024-12-20 14:25:34 +0900

    Add console log for user data retrieval in Header component

--- home/client/Header.vue
+++ home/client/Header.vue
@@ -70,6 +70,8 @@ class Header extends Vue {
   private async GetRightMenuItems(): Promise<MenuItem[]> {
     var user = await AuthUtil.GetUser<IUserAuthBase>();
 
+    console.log(user);
+
     if (user === null) {
       return [
         { label: "Login", link: "/Account/Login" },
